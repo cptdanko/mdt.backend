95bfa3f10ba9cea422e74c4ba3afe6ea9ac482b3
Bhuman Soni <bhuman@mydaytodo.com>
2023-09-13 21:06:47 +1000

    started adding social login via oauth

--- src/main/java/com/mydaytodo/web/backend/config/SecurityConfig.java
+++ src/main/java/com/mydaytodo/web/backend/config/SecurityConfig.java
@@ -2,19 +2,26 @@ package com.mydaytodo.web.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+    /**
+     * Ensure this security config blocks all access to the APIs
+     * and only allows access to the those public endpoints for
+     * registering and logging in.
+     * Rremove deprecated stuff
+     * @param http
+     * @return
+     * @throws Exception
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //return http
                 // other configuration options
-            ./*authorizeHttpRequests(authCustomizer -> authCustomizer
+            /*authorizeHttpRequests(authCustomizer -> authCustomizer
                     .requestMatchers(HttpMethod.POST, "/api/subscriptions").permitAll()
                     .requestMatchers(
                             "/api/signup", "/api/register", "/api/authenticate"
@@ -22,6 +29,13 @@ public class SecurityConfig {
                     .requestMatchers("/api/**").authenticated()
             )
             .build();*/
+        /*http.authorizeHttpRequests(Customizer.withDefaults())
+                        .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                            authorizationManagerRequestMatcherRegistry.requestMatchers("/api/register").permitAll()
+                                    .requestMatchers("/api/login").permitAll()
+                                    .requestMatchers("/api/todo/**").authenticated();
+
+                        }).build();*/
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/mydaytodo/web/backend/controller/SignupController.java
+++ src/main/java/com/mydaytodo/web/backend/controller/SignupController.java
@@ -1,15 +1,31 @@
 package com.mydaytodo.web.backend.controller;
 
+import com.mydaytodo.web.backend.models.User;
+import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class SignupController {
 
-    @GetMapping("/signup")
-    public ResponseEntity<String> signup(Quer)
+    @GetMapping("/register")
+    public ResponseEntity<HttpStatus> signup(@RequestBody User user) {
+        return new ResponseEntity<>(HttpStatusCode.valueOf(201));
+    }
+
+    /**
+     * Ensure this method caters for logging in using
+     * username/password to return a token
+     * logging in for oauth2 using Google
+     * logging in using username/password for basic auth
+     * @param user
+     * @return
+     */
+    @GetMapping("/login")
+    public ResponseEntity<HttpStatus> login(@RequestBody User user) {
+        //authenticate
+        return new ResponseEntity<>(HttpStatusCode.valueOf(200));
+    }
 }
